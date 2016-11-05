8f260374666f0ee69ca8c5a41a91c109c522bffc
shine636363 <shine636363@sina.com>
2016-11-05 13:34:56 +0000

    init overall structure

---
+++ run_CLT.m
@@ -0,0 +1,16 @@
+function run_CLT()
+%% Context learning based tracker across camera networks
+%
+%  Wei Liu (XXX); Jingjing Xiao (shine636363@sina.com)  Nov. 2016
+% 
+
+addpath(genpath('./'))
+
+for idf = 1: 100
+    % load image 
+    
+    % load detection
+    
+    % show detection
+    
+end
